8ed7b3c37f40308ac0be3a181efb2e6b406a9a60
Nolan Miller <156489804+nmiller15@users.noreply.github.com>
2024-01-31 01:49:42 -0500

    complete home page structure and function

---
+++ resources/js/anchorScroll.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => { 
+    anchor.addEventListener('click', function (e) { 
+        e.preventDefault(); 
+        const targetElement = document.querySelector(this.getAttribute('href')); 
+        window.scrollTo({ 
+            top: targetElement.offsetTop, 
+            behavior: 'smooth' 
+        }); 
+    }); 
+}); 
